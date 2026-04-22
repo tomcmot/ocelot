3e4990698219161a61ea5d21481f8f92ea6ac324
Tom Carstens <tomcarstens@gmail.com>
2026-04-22 14:46:11 -0500

    finish rudimentary value t ype inference

--- src/RelCBPV.hs
+++ src/RelCBPV.hs
@@ -3,6 +3,7 @@ module RelCBPV where
 import Control.Monad
 import Data.Map (Map)
 import Data.Map qualified as Map
+import Data.Bifunctor (second)
 
 data Kind
     = ValType
@@ -111,27 +112,75 @@ kindOf kenv = \case
         guard (bk == CompType)
         pure CompType
 
+checkKind :: KEnv -> Kind -> Type -> Maybe ()
+checkKind kenv k t = do
+    tk <- kindOf kenv t
+    guard (k == tk)
+
 
 substType :: String -> Type -> Type -> Type
 substType x witness = \case
     TyCon y | x == y -> witness
     t@(TyCon _) -> t
     TyUnit -> TyUnit
-    t -> t
 
+    TyExist y k t ->
+        if x == y then TyExist y k t
+        else TyExist y k (substType x witness t)
+    TyLam y k t ->
+        if x == y then TyLam y k t
+        else TyLam y k (substType x witness t)
+    TyForall y k t ->
+        if x == y then TyForall y k t
+        else TyForall y k (substType x witness t)
+    TyFix y t ->
+        if x == y then TyFix y t
+        else TyFix y (substType x witness t)
+
+    TyApply t1 t2 -> TyApply (substType x witness t1) (substType x witness t2)
+    TyThnk t -> TyThnk (substType x witness t)
+    TyPair t1 t2 -> TyPair (substType x witness t1) (substType x witness t2)
+    TyRet t -> TyRet (substType x witness t)
+    TyArrow t1 t2 -> TyArrow (substType x witness t1) (substType x witness t2)
+
+    TySum fields -> TySum (map (second (substType x witness)) fields)
+    TyProduct fields -> TyProduct (map (second (substType x witness)) fields)
 
 typeOfValue :: KEnv -> TyEnv -> Value -> Maybe Type
 typeOfValue kenv tenv = \case
     Var x -> Map.lookup x tenv
     Thunk c -> TyThnk <$> typeOfComp kenv tenv c
     Unit -> pure TyUnit
-    Pair a b -> TyPair 
+    Pair a b -> TyPair
         <$> typeOfValue kenv tenv a
         <*> typeOfValue kenv tenv b
-    Tag c v -> TySum . (:[]) . (c,) 
-        <$> typeOfValue kenv tenv v 
-    Pack t v x k a -> undefined
+    Tag c v -> TySum . (:[]) . (c,)
+        <$> typeOfValue kenv tenv v
+    Pack t v x k a -> do
+        vt <- typeOfValue kenv tenv v
+        let a' = substType x t a
+        guard (vt == a')
+        pure $ TyExist x k a
+
 
 typeOfComp :: KEnv -> TyEnv -> Comp -> Maybe Type
 typeOfComp kenv tenv = \case
-    _ -> undefined
+    Return v -> TyRet <$> typeOfValue kenv tenv v
+    Force v -> do
+        vt <- typeOfValue kenv tenv v
+        case vt of
+            TyThnk t -> pure t
+            _ -> Nothing
+    Match _ _ -> undefined
+    UnPair _ _ _ _ -> undefined
+    UnPack _ _ _ _ -> undefined
+    Lambda _ _ _ -> undefined
+    Bind _ _ _ -> undefined
+    Apply _ _ -> undefined
+    CoMatch _ -> undefined
+    ForAll _ _ _ -> undefined
+    Select _ _ -> undefined
+    ApplyTy _ _ -> undefined
+    Roll _ -> undefined
+    Unroll _ -> undefined
+    Fix _ _ -> undefined
